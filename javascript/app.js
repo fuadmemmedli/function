7d509a632e007244e80bfa2eb382027687e4704f
Fuad Memedli <fuadmemedli@Fuad-MacBook-Air.local>
2022-11-29 09:59:41 +0400

    BMI code added

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+function BMI(m,h){
+    let bmi = m / Math.pow(h,2);
+    let result;
+    if (bmi < 18.5) {
+        result = "az ceki";
+    } else if (18.5 < bmi && bmi < 24.9) {
+        result = "normal ceki";
+    } else if (25 < bmi && bmi < 29.9) {
+        result = "artiq ceki";
+    } else {
+        result = "odez";
+    }
+    return result;
+}
